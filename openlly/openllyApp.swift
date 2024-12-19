b32757d746b88c9efb6eea12f18021be9452e693
djsmk123 <djsmk123@gmail.com>
2024-12-19 17:22:01 +0530

    Initial Commit

---
+++ openlly/openllyApp.swift
@@ -0,0 +1,17 @@
+//
+//  openllyApp.swift
+//  openlly
+//
+//  Created by Mobin on 19/12/24.
+//
+
+import SwiftUI
+
+@main
+struct openllyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
